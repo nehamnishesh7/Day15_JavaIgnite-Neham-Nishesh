e46e5409e19233638461572d46593109b7a14a62
nehamnishesh7 <nisheshneham7@gmail.com>
2026-06-19 14:28:18 +0530

    Update Parking_lot_Management_System.java

--- Multithreading/Parking_lot_Management_System.java
+++ Multithreading/Parking_lot_Management_System.java
@@ -64,3 +64,51 @@ Main Method
 
 
 */
+class ParkingLot {
+    private int availableSlots = 3;
+
+    public synchronized void parkCar(String carName) {
+        if (availableSlots > 0) {
+            System.out.println(carName + " Parked");
+            availableSlots--;
+            System.out.println("Available Slots: " + availableSlots);
+        } else {
+            System.out.println("No Parking Available for " + carName);
+        }
+    }
+}
+
+class CarThread extends Thread {
+    private ParkingLot parkingLot;
+    private String carName;
+
+    public CarThread(ParkingLot parkingLot, String carName) {
+        this.parkingLot = parkingLot;
+        this.carName = carName;
+    }
+
+    @Override
+    public void run() {
+        parkingLot.parkCar(carName);
+    }
+}
+
+public class ShoppingMallParking {
+    public static void main(String[] args) {
+        ParkingLot lot = new ParkingLot();
+
+        CarThread car1 = new CarThread(lot, "Car 1");
+        CarThread car2 = new CarThread(lot, "Car 2");
+        CarThread car3 = new CarThread(lot, "Car 3");
+        CarThread car4 = new CarThread(lot, "Car 4");
+        CarThread car5 = new CarThread(lot, "Car 5");
+        CarThread car6 = new CarThread(lot, "Car 6");
+
+        car1.start();
+        car2.start();
+        car3.start();
+        car4.start();
+        car5.start();
+        car6.start();
+    }
+}
